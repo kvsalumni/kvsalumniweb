62fc9b3eea38b5490df22bee16f935759f5d71f9
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-27 01:32:19 +0530

    refactor ♻️: Updated president profile data interface
    - Added defining properties forPresident's name and photo portrait saving data as interface to contain profile president data so a new portable database interface framework profile base working component does appear consistent.scalablytypedReformulation in more simplified English context so other translators agree

---
+++ components/pages/landing/landing-database/presidentMessageData.ts
@@ -0,0 +1,9 @@
+export interface PresidentMessageData {
+  name: string;
+  image: string;
+}
+
+export const presidentMessageData: PresidentMessageData = {
+  name: "Er Maruti Nandan",
+  image: "/assets/images/president/President.jpeg"
+};
